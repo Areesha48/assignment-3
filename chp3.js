d3eb310c87e7b801981ccbbf92620efe626c3864
Areesha shehzad <157312844+Areesha48@users.noreply.github.com>
2024-05-18 23:17:21 +0500

    Add files via upload

---
+++ chp3.js
@@ -0,0 +1,24 @@
+//chap 3//
+// question 1//
+
+let age = "15 years old";
+alert(age);
+
+//question 2 //
+let visits = 0;
+visits ++;
+alert("you have visited this site" + visits + "times")
+// document .write("you have visited this site" + visits +"times");
+
+//question 3 //
+let birthyear = 1990;
+document .write("MY birth year is" + birthyear);
+document .write("<br>Data type of my declared variable is number");
+
+
+//Question 4 //
+let visitorName = "John Doe"; // Replace with actual visitor name
+  let productTitle = "T-shirt"; // Replace with actual product title
+  let quantity = 5; // Replace with actual quantity
+
+  document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.");
